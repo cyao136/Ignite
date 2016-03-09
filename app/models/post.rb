797d6261ea9969bd601733f21ddd15e3a6f6fb3c
CHENGYU YAO <chengyu2005@hotmail.com>
2016-03-09 11:03:06 -0800

    simple models with association created

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :project
+	has_many :comments, as: :commentable
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302182441) do
+ActiveRecord::Schema.define(version: 20160309184551) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "commentable_id",   limit: 4
@@ -103,6 +103,18 @@ ActiveRecord::Schema.define(version: 20160302182441) do
 
   add_index "pledges_users", ["pledge_id"], name: "index_pledges_users_on_pledge_id", using: :btree
   add_index "pledges_users", ["user_id"], name: "index_pledges_users_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "project_id", limit: 4
+    t.integer  "user_id",    limit: 4
+    t.integer  "type",       limit: 4
+    t.text     "message",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "posts", ["project_id"], name: "index_posts_on_project_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
