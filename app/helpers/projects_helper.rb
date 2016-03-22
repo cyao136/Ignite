e6780bee0f1269282e817fae92231055fe3963f9
cyao136 <chengyu2005@hotmail.com>
2016-03-22 09:39:21 -0700

    project controller

---
+++ app/helpers/projects_helper.rb
@@ -0,0 +1,10 @@
+module ProjectsHelper
+
+	####################################################
+	# project_id
+	# get the id of the current project instance
+	
+	def project_id
+		@project.id
+	end
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,24 @@ class Project < ActiveRecord::Base
 	has_many :video, as: :asset
 	has_many :pictures, as: :asset
 	has_and_belongs_to_many :genres
+	
+	enum state: [	:incomplete,
+					:stage_1_funding,
+					:stage_1_funded,
+					:stage_2_funding,
+					:stage_2_funded,
+					:archived
+					]
+					
+	####################################################
+	# start_stage_1
+	# Start the stage 1 funding process
+	
+	def start_stage_1
+		# validates the required fields
+		# TODO
+		
+		# set the state to stage_1_funding
+		self.state = "stage_1_funding"
+	end
 end
