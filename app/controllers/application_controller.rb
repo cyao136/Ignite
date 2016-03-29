8c2b55773be37f06ee76271fb12a01b376f9aaed
CHENGYU YAO <chengyu2005@hotmail.com>
2016-03-28 18:58:43 -0700

    authentication requirement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  # Actions require the user to be logged in
+  before_action :require_login
+  
+	def require_login
+		unless logged_in?
+			flash[:error] = "You must be logged in to access this section"
+			render "sessions/new" # halts request cycle
+		end
+	end
 end
