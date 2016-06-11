b66dc62de0f71d382cb2542ae1a03adadd97098b
CHENGYU YAO <chengyu2005@hotmail.com>
2016-06-10 21:02:18 -0700

    thredded

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,5 +1,5 @@
 class BrowseController < ApplicationController
-	skip_before_action :require_login
+	skip_before_filter :authenticate_user!
 
 	####################################################
 	# category
