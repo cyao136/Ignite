59a85787594eaec8c648882fde04a26e625b9d0e
Michael <michael620@gmail.com>
2016-06-15 19:34:11 -0700

    added initial support for facebook login using devise omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
   attr_accessor :remember_token, :activation_token, :reset_token
   # before_save   :downcase_fields
   # before_create :create_activation_digest
@@ -73,6 +73,25 @@ class User < ActiveRecord::Base
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Method that tries to find an existing user using omniauth
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.picture = auth.info.image # assuming the user model has an image
+    end
+  end
+
+  # Copy data from session whenever a user is initialized before sign up
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
   
   private
