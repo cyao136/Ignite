5f8d6efcaab97dce9e74fc95665b78a5bb14cca7
Michael Zhang <michael620@gmail.com>
2016-08-04 00:55:56 -0700

    updated gallery view for projects

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -60,6 +60,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [304, 171]
   end
 
+  version :galleryThumb do
+    process :resize_to_fill => [262.5, 262.5]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
