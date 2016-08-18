2cc55e5c39f1a2547ea4cf7919d9c14e74501664
Cheng Yu Yao <chengyu2005@hotmail.com>
2016-08-18 15:30:45 -0700

    minor css fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	def check_daily_login
-		if (current_user.current_sign_in_at.to_date - current_user.last_signed_in_at.to_date) >= 1
+		if (current_user.current_sign_in_at.to_date - current_user.last_sign_in_at.to_date) >= 1
 			if current_user.daily_login == "zero"
 				current_user.update_attribute(:daily_login, "one")
 			end
