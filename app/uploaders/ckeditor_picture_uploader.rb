67394f661385a29a9b7a282d9e032781324ed5e7
Cheng Yu Yao <chengyu2005@hotmail.com>
2016-08-26 15:54:34 -0700

    environment variable loading
    fix requirement faye.ru
    
    fix requirement faye.ru
    
    fix requirement faye.ru
    
    fix requirement faye.ru
    
    fix
    
    f

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -6,10 +6,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
-
-  # Choose what kind of storage to use for this uploader:
-  storage :fog
-
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
